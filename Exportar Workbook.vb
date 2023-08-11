58dc77ce0ef1e2fbae099eb19ff749a28e8db1de
Wesley Brass <138694362+wesleybrass@users.noreply.github.com>
2023-08-11 09:10:18 -0300

    Exportar-imprimir Workbook.vb

--- Exportar Workbook.vb
+++ Exportar Workbook.vb
@@ -1,18 +1,25 @@
 Sub ExportarWorkbook()
 	
-	'Declaração de variáveis
     Dim LoclArqv As Range
     Dim NameArqv As Range
     
 	'Definindo celulas com a informação necessária
     Set LoclArqv = Sheets("planilha1").Range("A1")
-    Set NameArqv = Sheets("planilha1").Range("B2)
-    
-	'Condicional
+    Set NameArqv = Sheets("planilha1").Range("B2")
+
     If NameArqv <> False Then
         ActiveWorkbook.SaveCopyAs LoclArqv & NameArqv
     End If
     
 End Sub
 
+Sub ExportarPDF()
 
+    localArquivo = ThisWorkbook.Path
+    nomeArquivo = "\nome_do_arquivo"
+    caminho = localArquivo + nomeArquivo
+
+    Sheets("Impressao").ExportAsFixedFormat Type:=xlTypePDF, fileName:=caminho, _
+    Quality:=xlQualityStandard, IncludeDocProperties:=False, OpenAfterPublish:=True
+    
+End Sub
